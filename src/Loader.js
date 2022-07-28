efb9cd6418cbb8d87d153b18fc7a9cc893fdcb02
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-07-28 21:23:11 +0300

    Корректировка компонента загрузчика.

--- src/Loader.js
+++ src/Loader.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default () => (
-<div style={{display: "flex", justifyContent: "center", margin: ".5rem"}}>
-    <div className="lds-dual-ring" />
-</div>)
+const Loader = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+      <div className="lds-dual-ring" />
+    </div>
+  );
+};
+
+export default Loader;

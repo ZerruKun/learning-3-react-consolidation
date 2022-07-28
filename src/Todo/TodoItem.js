fa7950382893773483b414cb6f6d5d3b42c87528
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-07-28 15:17:12 +0300

    Работа с библиотекой PropTypes.

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 const TodoItem = ({todo, index}) => {
   return (
@@ -8,5 +9,10 @@ const TodoItem = ({todo, index}) => {
   </li>
   );
 };
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number
+}
 
 export default TodoItem;

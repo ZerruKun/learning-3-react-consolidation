9c8aa812a977abad3f945d33d75afac3975d5d19
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-07-28 13:35:06 +0300

    Компонент TodoList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
   return (
-    <div>
-
+    <div className="wrapper">
+      <h1>React</h1>
+      <TodoList />
     </div>
   );
 }
